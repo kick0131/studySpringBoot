16c7221488944769fe0dd06fc68eaee16b4191ba
chihiro <chihiro0131@gmail.com>
2022-09-19 18:54:42 +0900

    aplus finished.

--- browsing_service/src/main/java/com/example/browsing_service/CreditApi.java
+++ browsing_service/src/main/java/com/example/browsing_service/CreditApi.java
@@ -1,7 +1,9 @@
 package com.example.browsing_service;
 
+import java.util.Map;
+
 interface CreditApi {
   public String loginsession = "";
-  String login(String loginurl, String id, String pass);
-  String getMonthlyData(String yyyymm);
+  void login(String loginurl, String id, String pass);
+  Map<Integer,String> getMonthlyData();
 }
--- browsing_service/src/main/java/com/example/browsing_service/CreditApiAplusImpl.java
+++ browsing_service/src/main/java/com/example/browsing_service/CreditApiAplusImpl.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -21,35 +23,37 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class CreditApiAplusImpl implements CreditApi {
 
   UserAccount user = null;
+  WebDriver driver = null;
 
   @RequestMapping("/")
-  public String getCreditInfo() {
+  public Map<Integer, String> getCreditInfo() {
     // TODO: パスパラメータで必要な情報を絞り込みできるように
     // seleniumSample();
     try {
       user = new UserAccount();
       login(user.model.url, user.model.id, user.model.password);
+      return getMonthlyData();
+      
     } catch (IOException e) {
       log.error(e.getMessage());
       e.printStackTrace();
     }
- 
-    return "hello world";
+
+    return null;
   }
 
-  @RequestMapping("/user")
+  @RequestMapping("/debug/user")
   public String userinfo() {
-    log.info("=====");
+    log.info("===== DEBUG Mode =====");
     try {
       user = new UserAccount();
     } catch (IOException e) {
       log.error(e.getMessage());
       e.printStackTrace();
     }
-    
+
     return "Success userinfo";
   }
-
 
   public void seleniumSample() {
     /// Seleniumの動作サンプル
@@ -75,9 +79,9 @@ public class CreditApiAplusImpl implements CreditApi {
   }
 
   @Override
-  public String login(String loginurl, String id, String pass) {
+  public void login(String loginurl, String id, String pass) {
 
-    WebDriver driver = new ChromeDriver();
+    driver = new ChromeDriver();
     driver.get(loginurl);
 
     // [login page]
@@ -85,29 +89,38 @@ public class CreditApiAplusImpl implements CreditApi {
     driver.findElement(By.id("webMemberId")).sendKeys(id);
     driver.findElement(By.id("webMemberPass")).sendKeys(pass);
     driver.findElement(By.xpath("//input[@value='ログイン']")).click();
-
-    // [top page]
-    // get parameter
-    // WebElement target = driver.findElement(By.xpath("//div[@id='change02']/h3")); // OK
-    WebElement target = driver.findElement(By.xpath("//div[@id='change02']//dd[1]")); // NG
-    System.out.println(target.getText());
-
-    // ng
-    // List<WebElement> costs = driver.findElements(By.xpath("//div[@class='dl-tbl-box-inner mb-15']/dl/dd"));
-    // log.info("costs.size:{}", costs.size());
-    // for (WebElement cost : costs) {
-    //   System.out.println(cost.getText());
-    // }
-    
-    // session end
-    driver.quit();
-    return null;
   }
 
   @Override
-  public String getMonthlyData(String yyyymm) {
-    // TODO Auto-generated method stub
-    return null;
+  public Map<Integer,String> getMonthlyData() {
+    // [top page]
+    // 格納先
+    Map<Integer, String> map = new HashMap<>();
+    // 4月開始
+    Integer month = 4;
+
+    // 月別情報の取得(4月開始)
+    List<WebElement> costs = driver.findElements(By.xpath("//div[@class='dl-tbl-box-inner mb-15']/dl/dd"));
+    log.info("costs.size:{}", costs.size());
+    for (WebElement cost : costs) {
+      String cost_str = cost.getAttribute("innerHTML").strip();
+      if (cost_str.isEmpty()) {
+        // pass
+      } else {
+        log.info(cost_str);
+        map.put(month, cost_str);
+      }
+      // 翌月の設定(4,5,6...12,1,2,3)
+      month = month + 1;
+      if (month == 13){
+        month = 1;
+      }
+    }
+
+    // session end
+    driver.quit();
+
+    return map;
   }
 
 }
