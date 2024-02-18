681827a750acf3514cbbc241bdd2b0c53176a947
chihiro <chihiro0131@gmail.com>
2024-02-18 16:31:27 +0900

    gradle版(プロジェクトのみ)

---
+++ springbootscheduler-gradle/src/main/java/com/example/springbootschedulergradle/SpringbootschedulerGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootschedulergradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootschedulerGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootschedulerGradleApplication.class, args);
+	}
+
+}
---
+++ springbootscheduler-gradle/src/test/java/com/example/springbootschedulergradle/SpringbootschedulerGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootschedulergradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootschedulerGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
